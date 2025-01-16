b2d556c14c57fc65cd9dcd0ba9730c3ea50398d6
nicofloflo <nico.e.flores1997@gmail.com>
2025-01-16 03:23:50 -0800

    Updated Flooded Only Mod

---
+++ Patches/Flooded.cs
@@ -0,0 +1,104 @@
+using HarmonyLib;
+using UnityEngine;
+
+namespace FloodedOnly.Patches;
+    
+
+public class Flood
+        {
+            [HarmonyPatch(typeof(StartOfRound),"SetPlanetsWeather")]
+            [HarmonyPrefix]
+            public static bool SetPlanetsWeather()
+            {
+                
+                foreach (SelectableLevel val in StartOfRound.Instance.levels)
+                {
+                    val.currentWeather = (LevelWeatherType)4;
+                    Debug.Log($"SetPlanetsWeather: {val.PlanetName + val.currentWeather}");
+                    
+                }
+                return false;
+            }
+
+            [HarmonyPatch(typeof(FloodWeather), "OnEnable")]
+            [HarmonyPrefix]
+            public static void SetToCurrentLevelWeather()
+            {
+                
+                if (TimeOfDay.Instance.currentLevel.levelID == 9)
+                {
+                    TimeOfDay.Instance.currentWeatherVariable = 0;
+                    Debug.Log("Lets Try");
+                    
+                }
+                if (TimeOfDay.Instance.currentLevel.levelID == 12)
+                {
+                    TimeOfDay.Instance.currentWeatherVariable = 0;
+                    Debug.Log("Lets Try");
+                    
+                }
+                if (TimeOfDay.Instance.currentLevel.levelID == 6)
+                {
+                    TimeOfDay.Instance.currentWeatherVariable = -18;
+                    Debug.Log("Lets Try");
+                    
+                }
+                        
+                
+            }
+
+            [HarmonyPatch(typeof(FloodWeather), "OnGlobalTimeSync")]
+            [HarmonyPrefix]
+
+            public static void SetSync()
+            {
+                if (TimeOfDay.Instance.currentLevel.levelID == 9)
+                {
+                    TimeOfDay.Instance.currentWeatherVariable2 = 3;
+                    Debug.Log("Lets Try");
+                    
+                }
+                if (TimeOfDay.Instance.currentLevel.levelID == 12)
+                {
+                    TimeOfDay.Instance.currentWeatherVariable2 = 1;
+                    Debug.Log("Lets Try");
+                    
+                }
+                if (TimeOfDay.Instance.currentLevel.levelID == 6)
+                {
+                    TimeOfDay.Instance.currentWeatherVariable2 = 3;
+                    Debug.Log("Lets Try");
+                    
+                }
+            }
+
+            [HarmonyPatch(typeof(FloodWeather), "Update")]
+            [HarmonyPrefix]
+            public static void Update()
+            {
+                if (TimeOfDay.Instance.currentLevel.levelID == 9)
+                {
+                    TimeOfDay.Instance.currentWeatherVariable = 0;
+                    TimeOfDay.Instance.currentWeatherVariable2 = 3;
+                    
+                    
+                }
+                if (TimeOfDay.Instance.currentLevel.levelID == 12)
+                {
+                    TimeOfDay.Instance.currentWeatherVariable = 0;
+                    TimeOfDay.Instance.currentWeatherVariable2 = 1;
+                    
+                    
+                }
+                if (TimeOfDay.Instance.currentLevel.levelID == 6)
+                {
+                    TimeOfDay.Instance.currentWeatherVariable = -18;
+                    TimeOfDay.Instance.currentWeatherVariable2 = 3;
+                    
+                    
+                }
+                
+            }
+        }
+
+                
---
+++ Plugin.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+using FloodedOnly.Patches;
+
+namespace FloodedOnly;
+
+[BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    
+    private const string PLUGIN_GUID = "FloodedOnly";
+    private const string PLUGIN_NAME = "Floodedonly";
+    private const string PLUGIN_VERSION = "1.0.1";
+
+
+
+    private readonly Harmony harmony = new Harmony(PLUGIN_GUID);
+   
+    private void Awake()
+    {
+        // Plugin startup logic
+        Debug.Log("I am a Cowboy");
+        harmony.PatchAll(typeof(Flood));
+        
+    }
+}
